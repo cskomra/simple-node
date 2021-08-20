3a4f880a8032f56b916f45a0998faa6810be155f
Connie Skomra <connie@cskomra.com>
2021-08-20 12:21:57 -0400

    use env var w/i the app

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const USER_NAME = process.env.DOCKER_USERNAME
+
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`Hello, ${USER_NAME}!`);
     await sleep(5000);
   }
 }
